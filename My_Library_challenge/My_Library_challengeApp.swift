820f9dde37d315cd9fe074eb5a230f0eb618ef12
clf94-dev <clf94@hotmail.es>
2023-06-24 16:51:55 +0200

    Initial Commit

---
+++ My_Library_challenge/My_Library_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_Library_challengeApp.swift
+//  My_Library_challenge
+//
+//  Created by Carmen Lucas on 24/6/23.
+//
+
+import SwiftUI
+
+@main
+struct My_Library_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
